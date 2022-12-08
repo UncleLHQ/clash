20a6aaa8b55af4c0b6e456b2eb5f3204bb3c795d
UncleLHQ <lhq1412@gmail.com>
2022-12-08 12:02:20 +0800

    ✨ reuse quic conn by session manager

--- adapter/outbound/trojan.go
+++ adapter/outbound/trojan.go
@@ -28,6 +28,8 @@ type Trojan struct {
 	gunTLSConfig *tls.Config
 	gunConfig    *gun.Config
 	transport    *http2.Transport
+
+	sessionManager sessionManager
 }
 
 type TrojanOption struct {
@@ -186,6 +188,20 @@ func (t *Trojan) IsByQuic() bool {
 }
 
 func (t *Trojan) DialQuicContext(ctx context.Context, opts []dialer.Option) (_ net.Conn, err error) {
+	session, err := t.sessionManager.getSession()
+	if err != nil {
+		return nil, err
+	}
+
+	stream, err := session.newStream()
+	if err != nil {
+		return nil, fmt.Errorf("%s quic failed to open stream with remote server connect error: %w", t.addr, err)
+	}
+
+	return stream, nil
+}
+
+func (t *Trojan) DialQuic(ctx context.Context, opts []dialer.Option) (quic.Connection, error) {
 	pConn, err := dialer.ListenPacket(ctx, "udp", "", opts...)
 	if err != nil {
 		return nil, fmt.Errorf("%s listen UDP at localhost once error: %w", t.addr, err)
@@ -200,15 +216,7 @@ func (t *Trojan) DialQuicContext(ctx context.Context, opts []dialer.Option) (_ n
 	if err != nil {
 		return nil, fmt.Errorf("%s connect error: %w", t.addr, err)
 	}
-
-	stream, err := c.OpenStream()
-	if err != nil {
-		return nil, fmt.Errorf("%s quic failed to open stream with remote server connect error: %w", t.addr, err)
-	}
-
-	return newStreamConn(c, stream, func() {
-		c.CloseWithError(0, "this conn is closing")
-	}), nil
+	return c, nil
 }
 
 func NewTrojan(option TrojanOption) (*Trojan, error) {
@@ -260,6 +268,25 @@ func NewTrojan(option TrojanOption) (*Trojan, error) {
 		t.gunConfig = &gun.Config{
 			ServiceName: option.GrpcOpts.GrpcServiceName,
 			Host:        tOption.ServerName,
+		}
+	} else if option.Network == "quic" {
+
+		quicDialFn := func() (quic.Connection, error) {
+			return t.DialQuic(context.Background(), t.Base.DialOptions())
+		}
+
+		newSession := func() (*session, error) {
+			return &session{
+				dialFn:     quicDialFn,
+				mutex:      new(sync.RWMutex),
+				updateTime: time.Now(),
+			}, nil
+		}
+
+		t.sessionManager = sessionManager{
+			ctx:        context.Background(),
+			newSession: newSession,
+			mutex:      new(sync.Mutex),
 		}
 	}
 
@@ -320,4 +347,88 @@ func newStreamConn(c quic.Connection, s quic.Stream, laterClose func()) *quicStr
 
 		laterClose: laterClose,
 	}
+}
+
+type session struct {
+	conn  quic.Connection
+	mutex *sync.RWMutex
+
+	dialFn func() (quic.Connection, error)
+
+	updateTime      time.Time
+	streamNum       int
+	closedStreamNum int
+}
+
+func (s *session) IsAvailable() bool {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	return s.streamNum <= 16 && time.Now().Sub(s.updateTime) < 15*time.Second
+}
+
+func (s *session) Close() error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	if s.conn != nil && s.streamNum == s.closedStreamNum {
+		err := s.conn.CloseWithError(0, "quic conn is closing")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *session) newStream() (*quicStreamConn, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.conn == nil {
+		var err error
+		s.conn, err = s.dialFn()
+		if err != nil {
+			return nil, err
+		}
+	}
+	stream, err := s.conn.OpenStream()
+	if err != nil {
+		return nil, err
+	}
+	s.streamNum++
+	return newStreamConn(s.conn, stream, s.closeStream), nil
+}
+
+func (s *session) closeStream() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.closedStreamNum++
+	if s.closedStreamNum == s.streamNum {
+		defer time.AfterFunc(10*time.Second, func() {
+			s.Close()
+		})
+	}
+}
+
+type sessionManager struct {
+	ctx context.Context
+
+	newSession func() (*session, error)
+	session    *session
+
+	mutex *sync.Mutex
+}
+
+func (p *sessionManager) getSession() (*session, error) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	if p.session != nil && p.session.IsAvailable() {
+		return p.session, nil
+	}
+
+	s, err := p.newSession()
+	if err != nil {
+		return nil, err
+	}
+
+	p.session = s
+	return s, nil
 }
